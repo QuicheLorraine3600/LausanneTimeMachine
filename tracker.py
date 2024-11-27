2c88d872f138de9cbdf2bf604ae3e085f4d40d03
André Santiago <82945862+Dresovense@users.noreply.github.com>
2024-11-27 16:51:59 +0100

    Revert "Merge branch 'main' of https://github.com/QuicheLorraine3600/LausanneTimeMachine"
    This reverts commit 26b1d263f954fc5f8af93c2eb65df95027267f4b, reversing
    changes made to 650a39115a5c35f612512f7281fd852523ee8437.

--- tracker.py
+++ tracker.py
@@ -135,11 +135,8 @@ def find_person(person: Person, people: Iterable[Person]):
 
 start_at = time()
 
-MODE = "normal"   # <--- to track people in 1855 
-MODE = "children" # <--- to track children in 1855
-
-
-people_1835 = process_people(recens_1835, mode=MODE)
+# people_1835 = process_people(recens_1835, mode="normal") # <--- to track people in 1855 
+people_1835 = process_people(recens_1835, mode="children") # <--- to track children in 1855
 
 MIN_BIRTH_YEAR = 1745
 MAX_BIRTH_YEAR = 1855
@@ -160,8 +157,6 @@ print("RESULTS: ")
 number_of_birth_year_mismatches = 0
 for pair in tracked:
 	print(pair)
-	if MODE == "children":
-		print(f"My job is {pair[1].job} and my father was a {pair[0].parent_job()}")
 	if pair[0].birth_year != pair[1].birth_year and pair[0].birth_year != None and pair[1].birth_year != None:
 		print("î Birth year mismatch")
 		number_of_birth_year_mismatches += 1
